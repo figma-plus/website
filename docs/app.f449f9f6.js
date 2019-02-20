70ae43ad35bc708b44d75f5001e458beb6bd4c1b
Mirko Santangelo <mirko@Mirkos-MBP.home>
2019-02-19 19:52:27 -0500

    Add Meta Image

--- docs/app.f449f9f6.js
+++ docs/app.f449f9f6.js
@@ -11040,7 +11040,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51179" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49618" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
